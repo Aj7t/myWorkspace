6c35b523fe9b95c1b4778b1f0a2c1a7b5fdf6d8a
Ajit Kumar <ajit.kumar@1silverbullet.tech>
2022-06-10 02:48:43 +0530

    edited

--- python/Oops/ineritance.py
+++ python/Oops/ineritance.py
@@ -1,8 +1,8 @@
 from main import*
 
 class rocky(animals):
-    def __init__(self, name, age):
-        super().__init__(name, age)
+    def __init__(self, name, age,color):
+        super().__init__(name, age,color)
         self.birthYear = 2019
 
     def about(myObj):
@@ -12,26 +12,26 @@ class leo(rocky):
     pass
 
 class teddy(rocky):
-    def __init__(self, name, age):
-        super().__init__(name, age)
+    def __init__(self, name, age,color):
+        super().__init__(name, age,color)
         print("this is multilevel inheritance")
 
 class bats(teddy,leo):
-    def __init__(self, name, age):
-        super().__init__(name, age)
+    def __init__(self, name, age,color):
+        super().__init__(name, age,color)
         print("This is multiple inheritance")
 
 
 #simple inheritence
-myobj2 = rocky("Mike", 12)
+myobj2 = rocky("Mike", 12, "brown")
 print(myobj2.about())
 print(myobj2.name , " BirthYear is " , myobj2.birthYear)
 
 # multilevel inheritance
-myobj3 = teddy("teddy", 20)
+myobj3 = teddy("teddy", 20,"brown")
 print(myobj3.about())
 
 # multiple inheritance
-myobj4 = bats("bats",12)
+myobj4 = bats("bats",12,"brown")
 print(myobj4.about())
 print(animals.mro())
